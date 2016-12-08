321acca9061c631979b2774a95c06a454f4ddc76
fanxl <flittlehappy@.163.com>
2016-12-08 10:05:55 +0800

    dyy

---
+++ myswift/myswift/netModel.swift
@@ -0,0 +1,14 @@
+//
+//  netModel.swift
+//  myswift
+//
+//  Created by FLE on 2016/12/8.
+//  Copyright © 2016年 FLE. All rights reserved.
+//
+
+import UIKit
+
+class netModel: NSObject {
+    var yyyy = 0
+    
+}
